7e0047f76c346f3dc653b8388ae61dec00f5ae18
bubkamisha14 <bubkamisha14@gmail.com>
2023-04-08 15:45:38 +0300

    add Footer component

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2023 Created by Mykhailo Bubka.</p>
+    </div>
+  );
+};
+
+export default Footer;
