146a76a417b887e8ffa9d5d689932530c17e2a24
bubkamisha14 <bubkamisha14@gmail.com>
2023-04-24 17:33:30 +0300

    add Footer component

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <p>© 2023 Created by Mykhailo Bubka.</p>
-    </div>
+    </footer>
   );
 };
 
